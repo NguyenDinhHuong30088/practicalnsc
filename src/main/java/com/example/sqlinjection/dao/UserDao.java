aae0dff7c3ea68398e742915b42405ef58294fca
NguyenDinhHuong30088 <70881463+NguyenDinhHuong30088@users.noreply.github.com>
2022-08-19 12:12:14 +0700

    Initial commit

--- src/main/java/com/example/sqlinjection/dao/UserDao.java
+++ src/main/java/com/example/sqlinjection/dao/UserDao.java
@@ -4,32 +4,25 @@ package com.example.sqlinjection.dao;
 import com.example.sqlinjection.model.User;
 import com.example.sqlinjection.utils.ConnectionUtils;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserDao {
     Connection connection = ConnectionUtils.getConnection();
 
     public User getUser(String pName, String password) {
         User user = null;
-//        String sql = "select * from users where username = '"+pName +"' and password = " +password;
-        String sql = "select * from users where username = ? and password = ?";
         try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setString(1, pName);
-            ps.setString(2, password);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()){
-                String name = rs.getString("username");
-                String pass = rs.getString("password");
+            Statement statement = connection.createStatement();
+            String sqlStatement = String.format("select * from users where username = '"+pName +"' and password = " +password);
+            ResultSet res = statement.executeQuery(sqlStatement);
+            while (res.next()){
+                String name = res.getString("username");
+                String pass = res.getString("password");
                 user = new User(name, pass);
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
         }
         return user;
     }
-
 }
--- src/main/java/com/example/sqlinjection/utils/ConnectionUtils.java
+++ src/main/java/com/example/sqlinjection/utils/ConnectionUtils.java
@@ -9,18 +9,20 @@ public class ConnectionUtils {
     private static Connection connection;
 
     public synchronized static Connection getConnection() {
-        String url = "jdbc:mysql://localhost:3306/NSC";
-        String user = "root";
-        String pass = "root";
+        String url = "jdbc:mysql://localhost:3306/nsc-practical";
+        String username = "root";
+        String password = "";
         if (connection == null) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
-                connection = DriverManager.getConnection(url, user, pass);
-            } catch (SQLException | ClassNotFoundException throwables) {
-                throwables.printStackTrace();
+                connection = DriverManager.getConnection(url, username, password);
+                System.out.println("Connect success !");
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
             }
+        }else {
+            System.out.println("Use existing connection !");
         }
-
         return connection;
     }
 
@@ -28,8 +30,8 @@ public class ConnectionUtils {
         try {
             connection.close();
             connection = null;
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         } finally {
             connection = null;
         }
